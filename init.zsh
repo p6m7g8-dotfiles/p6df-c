f193d79ad118b63e8126a274e2bb123f3628f79a
Philip M. Gollucci <pgollucci@p6m7g8.com>
2018-04-21 21:20:22 -0400

    Initial commit

---
+++ init.zsh
@@ -0,0 +1,35 @@
+p6df::modules::dev::version() { echo "0.0.1" }
+p6df::modules::dev::deps()    { 
+	ModuleDeps=()
+}
+
+p6df::modules::dev::external::brew() {
+
+  brew install autoconf
+  brew install automake
+  brew install cmake
+  brew install diff-so-fancy
+  brew install editorconfig
+  brew install gdb
+  brew install hg
+  brew install subversion
+  brew install swaks
+
+  brew install graphviz
+
+  brew install ansible
+  brew install docker
+  brew install docker-compose
+  brew install docker-machine
+
+  brew install terraform
+  brew install terraform-inventory
+  brew install terraform-provisioner-ansible
+
+}
+
+p6df::modules::dev::init() {
+
+}
+
+p6df::modules::dev::init
