5740a4d27b6056403700aec36e089bda7f6286bd
Philip M. Gollucci <pgollucci@p6m7g8.com>
2026-01-08 21:10:37 -0500

    feat(vscode): modernize vscode extensions (#12)

--- init.zsh
+++ init.zsh
@@ -21,18 +21,29 @@ p6df::modules::c::deps() {
 ######################################################################
 p6df::modules::c::vscodes() {
 
+  code --install-extension llvm-vs-code-extensions.vscode-clangd
+  code --install-extension ms-vscode.cmake-tools
+  code --install-extension twxs.cmake
+  code --install-extension ms-vscode.makefile-tools
   code --install-extension cschlosser.doxdocgen
   code --install-extension jeff-hykin.better-cpp-syntax
-  code --install-extension josetr.cmake-language-support-vscode
-  code --install-extension ms-vscode.cmake-tools
-  code --install-extension ms-vscode.cpptools
-  code --install-extension ms-vscode.cpptools-extension-pack
-  code --install-extension ms-vscode.cpptools-themes
-  code --install-extension ms-vscode.makefile-tools
-  code --install-extension twxs.cmake
 
   p6_return_void
 }
+
+#{
+#  "clangd.path": "clangd",
+#  "clangd.arguments": [
+#    "--background-index",
+#    "--clang-tidy",
+#    "--completion-style=detailed",
+#    "--header-insertion=iwyu",
+#    "--pch-storage=memory"
+#  ],
+#  "C_Cpp.intelliSenseEngine": "disabled",
+#  "C_Cpp.autocomplete": "disabled",
+#  "C_Cpp.errorSquiggles": "disabled"
+#}
 
 ######################################################################
 #<
