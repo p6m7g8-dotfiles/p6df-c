c76aab295574333e3d2136acc38ffae697ea9030
Philip M. Gollucci <pgollucci@p6m7g8.com>
2026-04-02 23:48:57 -0400

    refactor(module): remove unused prompt system function (#24)

--- init.zsh
+++ init.zsh
@@ -69,20 +69,3 @@ p6df::modules::c::external::brews() {
 
   p6_return_void
 }
-
-######################################################################
-#<
-#
-# Function: words c $CC = p6df::modules::c::prompt::system()
-#
-#  Returns:
-#	words - c $CC
-#
-#  Environment:	 CC
-#>
-######################################################################
-p6df::modules::c::prompt::system() {
-
-  p6_return_words 'c' "$"
-}
-
